4decd9b5d0c7cab0779c0120af1fb32fde8f537d
Emilio Dueñas <erduenas@espol.edu.ec>
2021-11-26 14:55:39 -0500

    Ventana de resultados

---
+++ app/src/main/java/com/example/evaluacion_g6/ventana2.java
@@ -0,0 +1,14 @@
+package com.example.evaluacion_g6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventana2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana2);
+    }
+}
